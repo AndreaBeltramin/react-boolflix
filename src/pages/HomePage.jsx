8e4240ef359f5dff835aa59ed996d021c3be690a
AndreaBeltramin <andreabeltra99@gmail.com>
2024-12-16 23:25:00 +0100

    fetch per tv Shows

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,16 +6,17 @@ const defaultFormData = {
 };
 
 export default function HomePage() {
+	//uso lo useState per controllare il cambiamento della lista e dell'input
 	const [list, setList] = useState([]);
+	const [listTvShows, setListTvShows] = useState([]);
 	const [formFields, setFormFields] = useState(defaultFormData);
 
+	//dichiaro la fetch per recuperare dall'endpoint dell'API le informazioni che mi servono
 	const fetchMovies = () => {
-		const url = `https://api.themoviedb.org/3/search/movie?query=${formFields.title}&include_adult=false&language=en-US&page=1`;
+		const url = `https://api.themoviedb.org/3/search/movie?query=${formFields.title}`;
 		const accessKey = import.meta.env.VITE_TMDB_API_KEY;
 
-		console.log(url);
-		console.log(accessKey);
-
+		//nell'header devo inserire l'API KEY per avere l'autorizzazione
 		const options = {
 			method: "GET",
 			headers: {
@@ -24,33 +25,71 @@ export default function HomePage() {
 			},
 		};
 
+		//faccio la fetch
 		fetch(url, options)
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data);
+				//recupero i dati di cui ho bisogno (titolo,lingua,voto) mappando results
 				const listMovies = data.results.map((movie) => ({
 					title: movie.title,
 					originalTitle: movie.original_title,
 					originalLanguage: movie.original_language,
 					vote: movie.vote_average,
 				}));
+				//setto la lista con i dati che ho recuperato
 				setList(listMovies);
-				console.log(listMovies, data.results);
+			})
+			.catch((err) => console.error(err));
+	};
+
+	//dichiaro la fetch per recuperare dall'endpoint dell'API le informazioni che mi servono sulle SERIE TV
+	const fetchTvShows = () => {
+		const url = `https://api.themoviedb.org/3/search/tv?query=${formFields.title}`;
+		const accessKey = import.meta.env.VITE_TMDB_API_KEY;
+
+		//nell'header devo inserire l'API KEY per avere l'autorizzazione
+		const options = {
+			method: "GET",
+			headers: {
+				accept: "application/json",
+				Authorization: accessKey,
+			},
+		};
+
+		//faccio la fetch
+		fetch(url, options)
+			.then((res) => res.json())
+			.then((data) => {
+				//recupero i dati di cui ho bisogno (titolo,lingua,voto) mappando results
+				const listTvShows = data.results.map((tvShow) => ({
+					title: tvShow.name,
+					originalTitle: tvShow.original_name,
+					originalLanguage: tvShow.original_language,
+					vote: tvShow.vote_average,
+				}));
+				//setto la lista con i dati che ho recuperato
+				setListTvShows(listTvShows);
 			})
 			.catch((err) => console.error(err));
 	};
 
 	// const { fetchMovies } = globalContext();
 
+	//dichiaro una funzione per tenere traccia del cambiamento dell'input
 	const handleInputChange = (e) => {
 		setFormFields({ ...formFields, [e.target.name]: e.target.value });
 	};
 
-	const handleFormSubmit = (e) => {
-		e.preventDefault();
-		fetchMovies();
+	//dichiaro una funzione che avviene al submit del form
+	const handleFormSubmit = (event) => {
+		event.preventDefault();
+		handleFetch();
 	};
 
+	const handleFetch = () => {
+		fetchMovies();
+		fetchTvShows();
+	};
 	return (
 		<div className="container mt-5">
 			<form onSubmit={handleFormSubmit}>
@@ -62,18 +101,29 @@ export default function HomePage() {
 					type="text"
 					className="me-5"
 				/>
-				<button className="btn btn-primary" onClick={() => fetchMovies()}>
-					Cerca
-				</button>
+				<button className="btn btn-primary">Cerca</button>
 			</form>
 			<ul>
-				{list.map((title, index) => (
+				{list.map((movie, index) => (
 					<li key={index}>
 						<ul>
-							<li>{title.title}</li>
-							<li>{title.originalTitle}</li>
-							<li>{title.originalLanguage}</li>
-							<li>{title.vote}</li>
+							<li>{movie.title}</li>
+							<li>{movie.originalTitle}</li>
+							<li>{movie.originalLanguage ? "lingua si" : "lingua no"}</li>
+							<li>{movie.vote}</li>
+						</ul>
+					</li>
+				))}
+			</ul>
+			<hr />
+			<ul>
+				{listTvShows.map((tvShow, index) => (
+					<li key={index}>
+						<ul>
+							<li>{tvShow.title}</li>
+							<li>{tvShow.originalTitle}</li>
+							<li>{tvShow.originalLanguage ? "lingua si" : "lingua no"}</li>
+							<li>{tvShow.vote}</li>
 						</ul>
 					</li>
 				))}
