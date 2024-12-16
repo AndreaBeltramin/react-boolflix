09ee521e93ae83f8a558d7b3daa9d3812a91fca1
AndreaBeltramin <andreabeltra99@gmail.com>
2024-12-16 17:39:03 +0100

    spostamento context

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { GlobalContextProvider } from "./contexts/globalContext";
 
 // import { useState } from 'react'
 import HomePage from "./pages/HomePage";
 
 function App() {
 	return (
-		<GlobalContextProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" Component={HomePage} />
-				</Routes>
-			</BrowserRouter>
-		</GlobalContextProvider>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" Component={HomePage} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
-
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { GlobalContextProvider } from "./contexts/globalContext";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/css/index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/index.css";
 
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
 
-import App from './App.jsx';
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+createRoot(document.getElementById("root")).render(
+	<GlobalContextProvider>
+		<StrictMode>
+			<App />
+		</StrictMode>
+	</GlobalContextProvider>
 );
